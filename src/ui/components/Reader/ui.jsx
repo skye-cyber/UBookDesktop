4659d2301aece318f8d7b928373c9554e661183a
Skye Wambua <swskye17@gmail.com>
2026-01-13 14:28:42 +0300

    implement simple, sleak custom scrollbar

--- src/ui/components/Reader/ui.jsx
+++ src/ui/components/Reader/ui.jsx
@@ -13,7 +13,7 @@ export const ReaderUI = ({ }) => {
                     <ReaderProgress />
                 </section>
 
-                <div id="reader-wrapper-container" className="relative h-[calc(100vh-7vh)] bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200 pb-3 px-6 md:px-0 md:pb-0.5 rounded-x-xl rounded-y-xl round-t-none max-w-3xl mx-auto my-[0vh] shadow-md leading-relaxed prose dark:prose-invert overflow-y-auto transition-colors duration-700 scrollbar-custom-v1">
+                <div id="reader-wrapper-container" className="relative h-[calc(100vh-7vh)] bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200 pb-3 px-6 md:px-0 md:pb-0.5 rounded-x-xl rounded-y-xl round-t-none max-w-3xl mx-auto my-[0vh] shadow-md leading-relaxed prose dark:prose-invert overflow-y-auto transition-colors duration-700 scrollbar-custom">
                     <ReaderContent />
                     <ToolTipUI />
                 </div>
