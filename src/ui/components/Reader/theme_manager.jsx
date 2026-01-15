726dda38d07bfc29f7c7aafd83ddb7f9c3d18d75
Skye Wambua <swskye17@gmail.com>
2026-01-15 18:48:46 +0300

    rename theme helper as theme_manager

---
+++ src/ui/components/Reader/theme_manager.jsx
@@ -0,0 +1,33 @@
+import { appState } from "./appState";
+import { StateManager } from "../../../renderer/js/syscore/StatesManager";
+
+const ThemeClassMap = {
+    'light': ['bg-white', 'text-gray-800'],
+    'dark': ['bg-gray-950', 'text-gray-200'],
+    'sepia': ['book-content'],
+    'night': ['bg-gray-900', 'text-gray-300'],
+    'tokyo-night': ['']
+}
+
+export class ThemeManager {
+    static changeTheme(theme) {
+        const readable = StateManager.get('readerSection')
+        // const themeSelector = readable?.getElementById('themeSelector');
+
+        // Remove all theme classes
+        readable.className = readable?.className.replace(/\b(bg-\w+|text-\w+)\b/g, '');
+
+        // Add base classes
+        readable?.classList.add('book-content', 'p-6', 'rounded-lg', 'border');
+
+        readable?.classList.remove(ThemeClassMap[appState.currentTheme])
+        readable?.classList.add(...ThemeClassMap[`${theme}`])
+
+        appState.currentTheme = theme;
+    }
+    static resetTheme() {
+        const readable = StateManager.get('readerSection')
+        readable.className = "react-portal-root select-text bg-white  dark:bg-gray-950 dark:border-gray-700 shadow-xl shadow-gray-300 rounded-lg selection:bg-[#ff007f]/20"
+    }
+}
+
