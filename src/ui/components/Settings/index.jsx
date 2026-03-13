6e7509bf6bc2f1a7010b72a8c7f3e70da00d54f4
skye <swskye17@gmail.com>
2026-03-13 19:18:41 +0300

    Update folder from src/renderer/css to src/styles
    Rename styles.css to index.css

---
+++ src/ui/components/Settings/index.jsx
@@ -0,0 +1,7 @@
+// src/renderer/components/Settings/index.js
+export { SettingsPage } from './SettingsPage';
+export { SettingsSection } from './SettingsSection';
+export { SettingsSwitch } from './SettingsSwitch';
+export { SettingsSelect } from './SettingsSelect';
+export { SettingsSlider } from './SettingsSlider';
+export { SettingsInput } from './SettingsInput';
