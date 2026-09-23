fc6646b429727e01187a1826a1bc12a0f2cd9d72
skye <swskye17@gmail.com>
2026-09-23 23:37:54 +0300

    Use white icon color for focus mode svg stroke

--- src/ui/components/Header/Header.tsx
+++ src/ui/components/Header/Header.tsx
@@ -187,7 +187,7 @@ export const Header = ({ }) => {
                             className="h-6 w-6"
                             viewBox="0 0 24 24"
                             fill="none"
-                            stroke="currentColor"
+                            stroke="white"
                             strokeWidth="2"
                             strokeLinecap="round"
                             strokeLinejoin="round"
